a8703093f7183fd9ddcf46088ea3fee98889cc39
PongsatornBoontham <161867621+PongsatornBT@users.noreply.github.com>
2025-02-23 11:42:38 +0700

    create example and structure

---
+++ next-story-w-daisy/stories/components/Button/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './../css/button.module.css';
+/** Primary UI component for user interaction */
+const Button = (props: ButtonProps) => {
+  const {
+    primary = false,
+    size = 'medium',
+    label,
+  } = props;
+  
+  const mode = primary ? style.storybook_button__primary : style.storybook_button__secondary;
+  return (
+    <button
+      type="button"
+      className={`${style.storybook_button} ${size} ${mode}`}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button
